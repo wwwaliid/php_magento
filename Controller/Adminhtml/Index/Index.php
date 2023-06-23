6d12e509ed12de2cdcc81f366a35bfe7ab448c4b
walid aiachi <waiachi@sqli.com>
2023-06-23 15:30:55 +0100

    admin login observer

--- Controller/Adminhtml/Index/Index.php
+++ Controller/Adminhtml/Index/Index.php
@@ -3,13 +3,15 @@
 namespace Training\AdminGridLogs\Controller\Adminhtml\Index;
 
 use Magento\Backend\App\Action;
-
+use Magento\Backend\App\Action\Context;
 use Magento\Framework\Controller\ResultFactory;
 
 class Index extends Action
 {
     public function execute()
     {
+        $test = 'oomgggggg';
+        consoleLog('helloooooooo' . ' this woooooorkssss' . ' ' . $test);
         return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
     }
 }
---
+++ Observer/SigninObserver.php
@@ -0,0 +1,47 @@
+<?php
+namespace Training\AdminGridLogs\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+use Training\AdminGridLogs\Model\UserActionFactory;
+use Training\AdminGridLogs\Model\ResourceModel\UserAction as UserActionResource;
+
+class SigninObserver implements ObserverInterface
+{
+    protected $UserActionFactory;
+    protected $UserActionResource;
+
+    public function __construct(UserActionFactory $UserActionFactory, UserActionResource $UserActionResource)
+    {
+        $this->UserActionFactory = $UserActionFactory;
+        $this->UserActionResource = $UserActionResource;
+    }
+
+    /**
+     * Save sign-in information to custom table.
+     * @param \Magento\Framework\Event\Observer $observer
+     */
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        $user = $observer->getUser();
+        if ($user && $user->getId()) {
+            $data = [
+                'login_date' => date('Y-m-d H:i:s'),
+                'username' => $user->getUserName(),
+                'session_id' => '', // Set session ID if available
+                'user_id' => $user->getId(),
+                'email' => $user->getEmail(),
+                'ip_address' => '', // Set IP address if available
+                'logout_date' => '', // Set logout date if available
+                'action_type' => 'signin', // Set the appropriate action type
+                'affected_module' => '', // Set the affected module if applicable
+            ];
+
+            $model = $this->UserActionFactory->create();
+
+            $model->setData($data);
+
+            $this->UserActionResource->save($model);
+
+        }
+    }
+}
