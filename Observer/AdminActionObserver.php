aa16278cad0b2840b8adf2282107a7ea8ef01641
walid aiachi <waiachi@sqli.com>
2023-07-06 17:25:03 +0100

    add log customers, products, categories, config, orders, users, stock

--- Observer/AdminActionObserver.php
+++ Observer/AdminActionObserver.php
@@ -28,6 +28,8 @@ class AdminActionObserver implements ObserverInterface
         $adminUser = $this->authSession->getUser();
         $controllerAction = $observer->getData('controller_action');
 
+        //dd($controllerAction->getRequest()->getFullActionName());
+
         if ($controllerAction->getRequest()->getFullActionName() === 'adminhtml_system_store_index') {
             $data = [
                 'login_date' => date('Y-m-d H:i:s'),
@@ -40,11 +42,130 @@ class AdminActionObserver implements ObserverInterface
                 'action_type' => 'list all stores!!', // Set the appropriate action type
                 'affected_module' => 'Stores', // Set the affected module if applicable
             ];
+            $model = $this->UserActionFactory->create();
+            $model->setData($data);
 
+            $this->UserActionResource->save($model);
         }
-        $model = $this->UserActionFactory->create();
-        $model->setData($data);
 
-        $this->UserActionResource->save($model);
+        if ($controllerAction->getRequest()->getFullActionName() === 'customer_index_index') {
+            $data = [
+                'login_date' => date('Y-m-d H:i:s'),
+                'username' => $adminUser->getUsername(),
+                'session_id' => '', // Set session ID if available
+                'user_id' => $adminUser->getId(),
+                'email' => $adminUser->getEmail(),
+                'ip_address' => '', // Set IP address if available
+                'logout_date' => '', // Set logout date if available
+                'action_type' => 'list all customers!!', // Set the appropriate action type
+                'affected_module' => 'Customers', // Set the affected module if applicable
+            ];
+            $model = $this->UserActionFactory->create();
+            $model->setData($data);
+
+            $this->UserActionResource->save($model);
+        }
+        if ($controllerAction->getRequest()->getFullActionName() === 'catalog_product_index') {
+            $data = [
+                'login_date' => date('Y-m-d H:i:s'),
+                'username' => $adminUser->getUsername(),
+                'session_id' => '', // Set session ID if available
+                'user_id' => $adminUser->getId(),
+                'email' => $adminUser->getEmail(),
+                'ip_address' => '', // Set IP address if available
+                'logout_date' => '', // Set logout date if available
+                'action_type' => 'list all products!!', // Set the appropriate action type
+                'affected_module' => 'Products', // Set the affected module if applicable
+            ];
+            $model = $this->UserActionFactory->create();
+            $model->setData($data);
+
+            $this->UserActionResource->save($model);
+        }
+        if ($controllerAction->getRequest()->getFullActionName() === 'catalog_category_index') {
+            $data = [
+                'login_date' => date('Y-m-d H:i:s'),
+                'username' => $adminUser->getUsername(),
+                'session_id' => '', // Set session ID if available
+                'user_id' => $adminUser->getId(),
+                'email' => $adminUser->getEmail(),
+                'ip_address' => '', // Set IP address if available
+                'logout_date' => '', // Set logout date if available
+                'action_type' => 'list all categories!!', // Set the appropriate action type
+                'affected_module' => 'Categories', // Set the affected module if applicable
+            ];
+            $model = $this->UserActionFactory->create();
+            $model->setData($data);
+
+            $this->UserActionResource->save($model);
+        }
+        if ($controllerAction->getRequest()->getFullActionName() === 'adminhtml_system_config_index') {
+            $data = [
+                'login_date' => date('Y-m-d H:i:s'),
+                'username' => $adminUser->getUsername(),
+                'session_id' => '', // Set session ID if available
+                'user_id' => $adminUser->getId(),
+                'email' => $adminUser->getEmail(),
+                'ip_address' => '', // Set IP address if available
+                'logout_date' => '', // Set logout date if available
+                'action_type' => 'Configuration Access!!', // Set the appropriate action type
+                'affected_module' => 'Configuration', // Set the affected module if applicable
+            ];
+            $model = $this->UserActionFactory->create();
+            $model->setData($data);
+
+            $this->UserActionResource->save($model);
+        }
+        if ($controllerAction->getRequest()->getFullActionName() === 'sales_order_index') {
+            $data = [
+                'login_date' => date('Y-m-d H:i:s'),
+                'username' => $adminUser->getUsername(),
+                'session_id' => '', // Set session ID if available
+                'user_id' => $adminUser->getId(),
+                'email' => $adminUser->getEmail(),
+                'ip_address' => '', // Set IP address if available
+                'logout_date' => '', // Set logout date if available
+                'action_type' => 'list all orders!!', // Set the appropriate action type
+                'affected_module' => 'Orders', // Set the affected module if applicable
+            ];
+            $model = $this->UserActionFactory->create();
+            $model->setData($data);
+
+            $this->UserActionResource->save($model);
+        }
+        if ($controllerAction->getRequest()->getFullActionName() === 'adminhtml_user_index') {
+            $data = [
+                'login_date' => date('Y-m-d H:i:s'),
+                'username' => $adminUser->getUsername(),
+                'session_id' => '', // Set session ID if available
+                'user_id' => $adminUser->getId(),
+                'email' => $adminUser->getEmail(),
+                'ip_address' => '', // Set IP address if available
+                'logout_date' => '', // Set logout date if available
+                'action_type' => 'list all users!!', // Set the appropriate action type
+                'affected_module' => 'Users', // Set the affected module if applicable
+            ];
+            $model = $this->UserActionFactory->create();
+            $model->setData($data);
+
+            $this->UserActionResource->save($model);
+        }
+        if ($controllerAction->getRequest()->getFullActionName() === 'inventory_stock_index') {
+            $data = [
+                'login_date' => date('Y-m-d H:i:s'),
+                'username' => $adminUser->getUsername(),
+                'session_id' => '', // Set session ID if available
+                'user_id' => $adminUser->getId(),
+                'email' => $adminUser->getEmail(),
+                'ip_address' => '', // Set IP address if available
+                'logout_date' => '', // Set logout date if available
+                'action_type' => 'list all stock!!', // Set the appropriate action type
+                'affected_module' => 'Stock', // Set the affected module if applicable
+            ];
+            $model = $this->UserActionFactory->create();
+            $model->setData($data);
+
+            $this->UserActionResource->save($model);
+        }
     }
 }
